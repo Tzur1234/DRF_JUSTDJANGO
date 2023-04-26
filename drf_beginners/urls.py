90c7cddb0cecfa79408841ea3a0a1decedea5ed3
root <root@DESKTOP-O1KC17T>
2023-04-26 22:30:37 +0300

    add static urls

--- drf_beginners/urls.py
+++ drf_beginners/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from posts import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/post/', include('posts.urls')),
 
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
